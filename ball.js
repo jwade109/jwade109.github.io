a7e448071062cbf3b437d8224d42af0ee84d610f
Wade Foster <jwade109@vt.edu>
2018-09-26 23:59:11 -0400

    Revamp front-end

--- ball.js
+++ ball.js
@@ -1,158 +1,41 @@
 class Ball
 {
-    constructor(x, y, r, g, vx, vy, damp)
+    constructor(x, y, vx, vy)
     {
         this.x = x;
         this.y = y;
-        this.r = r;
-        this.g = g;
+        this.r = 1;
 
         this.vx = vx;
         this.vy = vy;
-        this.damp = damp;
-        this.colors = [];
 
         this.theta = 0;
         this.omega = Math.random()*4 - 2;
     }
 
-    draw(canvas)
-    {
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r,
-            this.theta, 2*Math.PI+this.theta);
-        ctx.globalAlpha=0.8;
-        ctx.fillStyle="black";
-        ctx.stroke();
-
-        ctx.globalAlpha=0.3;
-        var arclength = 2*Math.PI/this.colors.length;
-
-        for (var i = 0; i < this.colors.length; ++i)
-        {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.r,
-                this.theta+(arclength*i),
-                this.theta+(arclength*(i+1)));
-            ctx.lineTo(this.x, this.y);
-            ctx.fillStyle=this.colors[i];
-            ctx.fill();
-        }
-
-        if (this.colors.length == 0)
-        {
-            ctx.fillStyle="gray";
-            ctx.fill();
-        }
-    }
-
-    addColor(color)
-    {
-        this.colors.push(color);
-    }
-
-    erase(canvas)
-    {
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
-        ctx.stroke();
-    }
-
-    update(dt, canvas)
+    kinematics(dt, width, height)
     {
         this.theta += this.omega*dt;
 
-        if (this.x + this.r >= canvas.width)
-        {
-            this.vx = -Math.abs(this.vx)*this.damp;
-            this.vy *= this.damp;
-        }
+        if (this.x + this.r >= width)
+            this.vx = -Math.abs(this.vx);
         if (this.x - this.r <= 0)
-        {
-            this.vx = Math.abs(this.vx)*this.damp;
-            this.vy *= this.damp;
-        }
-        if (this.y + this.r >= canvas.height)
-        {
-            this.vy = -Math.abs(this.vy)*this.damp;
-            this.vx *= this.damp;
-        }
-        if (this.y - this.r <= 0)
-        {
-            this.vy = Math.abs(this.vy)*this.damp;
-            this.vx *= this.damp;
-        }
+            this.vx = Math.abs(this.vx);
+        if (this.y + this.r >= height)
+            this.vy = -Math.abs(this.vy);
+        if (this.y - this.r <= 0) 
+            this.vy = Math.abs(this.vy);
 
         this.x += this.vx*dt;
-        if (this.x + this.r > canvas.width)
-            this.x = canvas.width - this.r;
-        this.y += this.vy*dt;
-        if (this.y + this.r > canvas.height)
-            this.y = canvas.height - this.r;
-        if (this.y + this.r < canvas.height)
-            this.vy += this.g*dt;
+        if (this.x + this.r > width)
+            this.x = width - this.r;
+        if (this.x - this.r < 0)
+            this.x = this.r;
 
-        if (Math.abs(this.y + this.r - canvas.height) < 10 &&
-            Math.abs(this.vy) < 50)
-        {
-            this.y = canvas.height - this.r;
-            this.vy = 0;
-        }
+        this.y += this.vy*dt;
+        if (this.y + this.r > height)
+            this.y = height - this.r;
+        if (this.y - this.r < 0)
+            this.y = this.r;
     }
 }
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function ballot2ball(ballot)
-{
-    var b = new Ball(0, 0, 0, 0, 0, 0, 0, 0);
-    b.r = ballot.weight;
-    for (var c in ballot.candidates)
-        b.addColor(str2color(ballot.candidates[c]));
-    return b;
-}
-
-var fps = 50;
-
-canvas = document.getElementById("canvas");
-width = document.body.clientWidth;
-height = document.body.clientHeight;
-var balls = [];
-
-// for (var i = 0; i < 100; ++i)
-// {
-    // balls.push(new Ball(
-
-    // for (var j = 0; j < Math.random()*8; ++j)
-    // {
-        // balls[i].addColor(getRandomColor());
-    // }
-// }
-
-function draw()
-{
-    setTimeout(function()
-    {
-        var ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.canvas.width  = document.body.clientWidth;
-        ctx.canvas.height = document.body.scrollHeight;
-        requestAnimationFrame(draw);
-        for (var b in balls)
-        {
-            balls[b].update(1/fps, canvas);
-            balls[b].draw(canvas);
-        }
-    }, 1000/fps);
-}
-
-draw();
---
+++ marx.js
@@ -0,0 +1,207 @@
+function getRandomColor()
+{
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++)
+    {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+color_palette = ["Blue", "Red", "Yellow", "Green",
+                 "Orange", "Olive", "Purple", "Fuchsia",
+                 "Lime", "Teal", "Aqua", "Maroon", "Navy"];
+
+function str2color(str)
+{
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }    
+    var hex = ((hash>>24)&0xFF) + ((hash>>16)&0xFF) +
+              ((hash>>8)&0xFF) + (hash&0xFF);
+    var index = hex % color_palette.length;
+    return color_palette[index];
+}
+
+function getColors(ballots)
+{
+    var colors = {};
+    var count = 0;
+    ballots.forEach(function(b)
+    {
+        for (var c in b.candidates)
+        {
+            if (typeof colors[b.candidates[c]] === 'undefined')
+            {
+                colors[b.candidates[c]] = color_palette[count];
+                count = (count + 1) % color_palette.length;
+            }
+        }
+    });
+    return colors;
+}
+
+function clickGetWinner()
+{
+    elem = document.getElementById('winner');
+    if (ballot_box.length)
+    {
+        var winner = getRunoffWinner(ballot_box)
+        document.getElementById("winner")
+            .parentElement.style.display = "inline-block";
+        elem.innerHTML = 
+        "<h3>The winner is " + winner + "!</h3>";
+    }
+}
+
+function clickClear()
+{
+    document.getElementById("winner")
+        .parentElement.style.display = "none";
+    document.getElementById("candidates")
+        .parentElement.style.display = "none";
+    ballot_box = [];
+    document.getElementById('winner').innerHTML = "";
+    document.getElementById('candidates').innerHTML = "";
+}
+
+function clickAddBallot()
+{
+    var x = document.getElementById('input');
+    var text = x.elements[0].value;
+    var count = parseInt(x.elements[1].value);
+
+    if (isNaN(count)) count = 1;
+    
+    if (text != "")
+    {
+        for (var i = 0; i < count; ++i)
+        {
+            addBallot(Ballot.fromString(text));
+        }
+    }
+}
+
+function addBallot(ballot)
+{
+    document.getElementById("candidates")
+        .parentElement.style.display = "inline-block";
+    ballot_box.push(ballot);
+    makeBalls(ballot_box, width, height);
+    colors = getColors(ballot_box);
+    var cand = document.getElementById('candidates');
+    cand.innerHTML = "";
+    for (var c in colors)
+    {
+        cand.innerHTML +=
+            "<p style='" + 
+            "font-size: 24px; " +
+            "margin: 10px 0; " +
+            "padding: 0 0 0 15px; " + 
+            "border-left: 24px solid " + colors[c] + "'>" +
+            c + "</p>";
+    }
+}
+
+function drawBallot(ballot, canvas)
+{
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+
+    var arclength = 2*Math.PI/ballot.candidates.length;
+
+    for (var i = 0; i < ballot.candidates.length; ++i)
+    {
+        ctx.beginPath();
+        ctx.arc(ballot.ball.x, ballot.ball.y, ballot.weight*30,
+            ballot.ball.theta+(arclength*i),
+            ballot.ball.theta+(arclength*(i+1)));
+        if (ballot.candidates.length > 1)
+            ctx.lineTo(ballot.ball.x, ballot.ball.y);
+        ctx.globalAlpha = 0.2;
+        ctx.fillStyle = colors[ballot.candidates[i]];
+        ctx.fill();
+    }
+    for (var i = 0; i < ballot.candidates.length; ++i)
+    {
+        ctx.beginPath();
+        ctx.arc(ballot.ball.x, ballot.ball.y, ballot.weight*30,
+            ballot.ball.theta+(arclength*i),
+            ballot.ball.theta+(arclength*(i+1)));
+        if (ballot.candidates.length > 1)
+            ctx.lineTo(ballot.ball.x, ballot.ball.y);
+        ctx.globalAlpha = 0.7;
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 1;
+        ctx.stroke();
+    }
+}
+
+function makeBalls(ballots, width, height)
+{
+    ballots.forEach(function(b)
+    {
+        if (typeof b.ball === 'undefined')
+        {
+            b.ball = new Ball(Math.random()*3*width/4 + width/8,
+                              Math.random()*3*height/4 + height/8,
+                              Math.random()*500 - 250,
+                              Math.random()*500 - 250);
+        }
+    });
+}
+
+function draw(width, height)
+{
+    setTimeout(function(width, height)
+    {
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.canvas.width = document.body.clientWidth;
+        ctx.canvas.height = document.body.scrollHeight;
+        width = ctx.canvas.width;
+        height = ctx.canvas.height;
+        requestAnimationFrame(draw);
+        ballot_box.forEach(function(b)
+        {
+            b.ball.r = b.weight*30;
+            b.ball.kinematics(1/fps, width, height);
+            drawBallot(b, canvas);
+        });
+    }, 1000/fps);
+}
+
+var fps = 50;
+var width = document.body.clientWidth;
+var height = document.body.clientHeight;
+var canvas = document.getElementById("canvas");
+var ballot_box = [];
+
+sitecode = document.URL.split("#")[1];
+
+if (sitecode == "load")
+{
+    for (i = 0; i < 15; i++)
+    {
+        addBallot(new Ballot("Asparagus", "Beans", "Corn", "Dill"));
+    }
+    for (i = 0; i < 12; i++)
+    {
+        addBallot(new Ballot("Beans", "Asparagus", "Corn", "Dill"));
+    }
+    for (i = 0; i < 11; i++)
+    {
+        addBallot(new Ballot("Dill", "Beans", "Asparagus"));
+    }
+    for (i = 0; i < 8; i++)
+    {
+        addBallot(new Ballot("Beans"));
+    }
+}
+
+colors = getColors(ballot_box);
+makeBalls(ballot_box, width, height);
+draw(width, height);
+clickAddBallot();
