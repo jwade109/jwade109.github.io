bb98df9f58786666745a30920032b7c54a769c7c
Wade Foster <jwade109@vt.edu>
2019-04-25 01:13:46 -0400

    Add construction duck

--- scripts/keep-talking/module-select.js
+++ scripts/keep-talking/module-select.js
@@ -4,14 +4,17 @@ select.innerHTML =
     "<div style='height: 30px'></div>" +
     "<select id='module' onchange='loadnew()'>" +
       "<option selected>Load a new Module</option>" +
-      "<option value='morse-code'>Morse Code</option>" +
-      "<option value='passwords'>Passwords</option>" +
+      "<option value='home'>See all modules</option>" +
+      "<option value='morse-code.html'>Morse Code</option>" +
+      "<option value='passwords.html'>Passwords</option>" +
+      "<option value='mazes.html'>Mazes</option>" +
     "</select>";
 
 let module = document.getElementById("module");
 
 function loadnew()
 {
-    console.log("Switching to " + module.value);
-    window.location.href = module.value;
+    console.log(module.value);
+    if (module.value == 'home') window.location.href = '../keep-talking.html';
+    else window.location.href = module.value;
 }
