782f98f3c95298292b7b66d4858218d3d8859403
Wade Foster <jwade109@vt.edu>
2019-05-03 01:46:17 -0400

    ++version

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
 document.getElementById("version").innerHTML =
-    "2019.04.25c" + " - <a href='/'>Home</a><br><br>" +
+    "2019.05.03a" + " - <a href='/'>Home</a><br><br>" +
     "<img src='/resources/favicon.ico' height=25px></img>";
