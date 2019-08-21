ee6abfca1283da85765835749fdaea601645e90c
Wade Foster <jwade109@vt.edu>
2019-08-21 17:43:05 -0400

    Add A* info

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
 document.getElementById("version").innerHTML =
-    "2019.05.03b" + " - <a href='/'>Home</a><br><br>" +
+    "2019.08.21a" + " - <a href='/'>Home</a><br><br>" +
     "<img src='/resources/favicon.ico' height=25px></img>";
