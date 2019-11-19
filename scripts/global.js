16e7831d2b100f7ceb106f3c749a03a48d7893be
Wade Foster <jwade109@vt.edu>
2019-11-19 12:14:38 -0500

    Update gpa

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
 document.getElementById("version").innerHTML =
-    "2019.08.21a" + " - <a href='/'>Home</a><br><br>" +
+    "2019.11.19a" + " - <a href='/'>Home</a><br><br>" +
     "<img src='/resources/favicon.ico' height=25px></img>";
