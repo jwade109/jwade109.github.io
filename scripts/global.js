263548d7924cd09899a4462f84ac729993cf4423
Wade Foster <jwade109@vt.edu>
2020-09-22 00:47:20 -0400

    Update version

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
 document.getElementById("version").innerHTML =
-    "2019.11.19a" + " - <a href='/'>Home</a><br><br>" +
+    "2020.09.22a" + " - <a href='/'>Home</a><br><br>" +
     "<img src='/resources/favicon.ico' height=25px></img>";
