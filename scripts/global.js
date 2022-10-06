b125dd61242d2f94f1bbfdcf771ec9379d19d587
Wade Foster <jwade109@vt.edu>
2022-10-05 23:04:37 -0400

    Update resume so it's not an embarrassment.

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,2 @@
 document.getElementById("version").innerHTML =
-    "2022.10.05a" + " - <a href='/'>Home</a>";
+    "2022.10.05a" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
