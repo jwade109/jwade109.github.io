6e518b8e14a26a7d595e83d92936e1d611242ed1
Wade Foster <jwade109@vt.edu>
2024-02-17 16:20:00 -0500

    Woops

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,2 @@
 document.getElementById("version").innerHTML =
-    "2024.02.17b" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
+    "2024.02.17c" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
