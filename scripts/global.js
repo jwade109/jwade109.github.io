4cb706673629e6d005cee978ea3e1463f98ae80b
Wade Foster <jwade109@vt.edu>
2025-10-21 18:30:32 -0400

    Add launch video

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,2 @@
 document.getElementById("version").innerHTML =
-    "2025.10.21a" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
+    "2025.10.21b" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
