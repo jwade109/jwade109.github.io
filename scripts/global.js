c78dff703cb787b30eb57ade488a8ecdcb4877bf
Wade Foster <jwade109@vt.edu>
2025-10-21 18:08:28 -0400

    Add orbits

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
 document.getElementById("version").innerHTML =
-    "2022.08.26a" + " - <a href='/'>Home</a><br><br>" +
+    "2025.10.21a" + " - <a href='/'>Home</a><br><br>" +
     "<img src='/resources/favicon.ico' height=25px></img>";
