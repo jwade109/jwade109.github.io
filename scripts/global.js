4920fc8aa4bf3acd485c77eb362262bb8487d519
Wade Foster <jwade109@vt.edu>
2026-01-04 16:05:11 -0500

    Upload vertex 2026.01.04

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,2 @@
 document.getElementById("version").innerHTML =
-    "2025.12.16a" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
+    "2026.01.04a" + " / <a href='/' style='font-family: consolas, monospace'>cd ~</a>";
